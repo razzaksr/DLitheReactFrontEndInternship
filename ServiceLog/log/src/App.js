54f6435f554b8cc72bb61486b26ceb69dc88de5f
razzaksr <razzaksr@gmail.com>
2021-07-24 01:03:22 +0530

    POC Done insert, edit,delete,list,read

--- ServiceLog/log/src/App.js
+++ ServiceLog/log/src/App.js
@@ -3,7 +3,6 @@ import ReactDom, { render } from "react-dom";
 import { Route, Switch } from "react-router";
 import { Link, Redirect } from "react-router-dom";
 import ContactUs from "./ContactUs";
-import EdiCheck from "./EdiCheck";
 import Edit from "./Edit";
 import Home from "./Home";
 import InTake from "./InTake";
@@ -34,13 +33,9 @@ const App=()=>{
         <Route path="/newuser" exact component={()=><NewUser/>}/>
         <Route path="/contact" exact component={()=><ContactUs/>}/>
         <Route path="/in" exact component={()=><InTake/>}/>
-<<<<<<< HEAD
         <Route path="/view" exact component={()=><List onDelete={del}/> }/>
         {/* <Route path="/edit" exact component={()=><Edit data={bike}/>}/> */}
         {/* <Route path="/editcheck/:reg" exact component={()=><EdiCheck/>}/> */}
-=======
-        <Route path="/view" exact component={()=><List/>}/>
->>>>>>> 2161835c3f87f0cba35ebe37ce694cbd52f4d6ac
       </Switch>
     </>
   );
--- ServiceLog/log/src/ContactUs.js
+++ ServiceLog/log/src/ContactUs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {TextField,Button} from "@material-ui/core";
@@ -71,78 +70,4 @@ const ContactUs=()=>{
     )
 }
 
-=======
-import React from "react";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {TextField,Button} from "@material-ui/core";
-import SendIcon from '@material-ui/icons/Send';
-import Send from "@material-ui/icons/Send";
-import  "react-icons/fa";
-import { FaFacebook, FaGooglePlusG, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-
-const ContactUs=()=>{
-    return(
-        <>
-            <div className="container-fluid">
-                <div className="mt-lg-5 row justify-content-center">
-                    <div className="col-md-6 col-sm-12">
-                        <form className="rounded p-3 shadow">
-                        <TextField
-                        id="outlined-basic" 
-                        label="Name" 
-                        variant="outlined"
-                        className="form-control mb-3" 
-                        name="name"
-                        />
-                        
-                        <TextField
-                        id="outlined-basic" 
-                        label="Contact" 
-                        className="form-control mb-3" 
-                        variant="outlined"
-                        name="contact"
-                        />
-
-                        <TextField
-                        id="outlined-basic" 
-                        label="Email" 
-                        className="form-control mb-3" 
-                        variant="outlined"
-                        name="email"
-                        />
-                        <textarea
-                        name="messgae"
-                        className="form-control"
-                        placeholder="Message"
-                        />
-                        <div className="mt-5 row justify-content-around">
-                            <Button className="col-2" variant="outlined" color="primary">
-                                <Send/>
-                            </Button>
-                        </div>
-                        </form>
-                    </div>
-                    <div className="col-md-6 col-sm-12">
-                        <div className="row justify-content-around">
-                            <a className="col" href="https://www.facebook.com/Dlitheconsultancyservices/">
-                                <FaFacebook color="#3b5998" style={{height:'200px',width:'250px'}}/>
-                            </a>
-                            <a className="col" href="https://www.linkedin.com/company/dlithe?originalSubdomain=in">
-                                <FaLinkedin color="#0077b5" style={{height:'200px',width:'250px'}}/>
-                            </a>
-                        </div>
-                        <div className="row justify-content-around">
-                            <a className="col" href="https://www.facebook.com/Dlitheconsultancyservices/">
-                                <FaTwitter color="#00acee" style={{height:'200px',width:'250px'}}/>
-                            </a>
-                            <FaGooglePlusG className="col" href="https://www.linkedin.com/company/dlithe?originalSubdomain=in" style={{height:'200px',width:'250px'}}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
->>>>>>> 2161835c3f87f0cba35ebe37ce694cbd52f4d6ac
 export default ContactUs;
